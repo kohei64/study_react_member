ce94cead6a370eacd73494aa7f7d8b7785820a9b
kohei <kohei3400@gmail.com>
2022-06-11 18:35:31 +0900

    delete:create-component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignUp from "./components/SignUp/SignUp"
 import LoginForm from "./components/LoginForm/LoginForm";
 import Home from "./components/Home/Home";
-import Create from './components/Create/Create'
 import Account from "./components/Account/Account";
 import Update from './components/Account/Update'
 import Members from "./components/Members/Members";
@@ -16,7 +15,6 @@ const App = () => {
         <Route path={"/signup"} element={<SignUp />}></Route>
         <Route path={"/login"} element={<LoginForm />}></Route>
         <Route path={"/"} element={<Home />}></Route>
-        <Route path={"/create"} element={<Create />}></Route>
         <Route path={"/account/:id"} element={<Account />}></Route>
         <Route path={"account/:id/update"} element={<Update />}></Route>
         <Route path={"/members"} element={<Members />}></Route>
