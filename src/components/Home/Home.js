6cc3e000354a43b6db803b53aa9a8adac4bb30c2
kohei <kohei3400@gmail.com>
2022-06-12 14:06:07 +0900

    add:homeItem

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
+import api from "../api/api";
+import HomeItem from "./HomeItem"
 
 const Home = () => {
+  const { id } = useParams(); //URLのidを代入
+  const [user, setUser] = useState([]);
+
+  // api/ユーザー取得
+  React.useEffect(() => {
+    api.get(`/user/${id}`).then((res) => {
+      setUser(res.data);
+    });
+    // eslint-disable-next-line
+  }, []);
+
   return (
     <div>
       <Navbar />
-      <h1 className="">Home</h1>
+      <HomeItem 
+      user={user}
+      />
     </div>
   );
 };
